0ef5160bd66acf13d03a43c79d35239fec4220bb
dlgydyd123 <73455924+dlgydyd123@users.noreply.github.com>
2024-08-05 18:10:26 +0900

    Add files via upload

---
+++ Ex_0805/src/ex03_method/Start.java
@@ -0,0 +1,16 @@
+package ex03_method;
+
+public class Start {
+	 int count = 0;
+	 int a = (int) ((Math.random()*50)+1);
+	 String upDown(int num) {
+		 count++;
+		 if(num>a) {
+			 return "Down!";
+		 }else if(num<a){
+			 return "Up!";
+		 }else{
+			 return "정답 "+count+"번째에 맞추셨습니다.";
+		 }
+	 }
+}
